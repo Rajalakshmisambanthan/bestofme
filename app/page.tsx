d2bb52097a5943ae28b4451337d07c0ec7af12b7
Rajalakshmisambanthan <rajalakshmisambanthan@gmail.com>
2024-10-23 13:10:54 +0530

    rajalakshmi

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,32 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
+"use client"
 
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import {Tabs, Tab} from "@nextui-org/react";
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Make&nbsp;</span>
-        <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
-        <br />
-        <span className={title()}>
-          websites regardless of your design experience.
-        </span>
-        <div className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
-        </div>
+    <div className="bg-[#D9E8D9]">
+      <div>
+      <Image
+        src="/images/Union.png"
+        width={49}
+        height={43}
+        alt="Picture of the author"
+      />
       </div>
-
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href={siteConfig.links.docs}
-        >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
+      <Tabs variant="underlined" aria-label="Tabs variants">
+          <Tab  title="Photos"/>
+          <Tab  title="Music"/>
+          <Tab  title="Videos"/>
+        </Tabs>
+        <div>
+        <Image
+          src="/images/Vector.png"
+          width={48}
+          height={48}
+          alt="Picture of the author"
+        />
       </div>
-
-      <div className="mt-8">
-        <Snippet hideCopyButton hideSymbol variant="bordered">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-        </Snippet>
-      </div>
-    </section>
+    </div>
   );
 }
